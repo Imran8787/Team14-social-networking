dcf95388602461c2dba14c7d513cfaf0742971ae
imran8787 <imranbadesab87@gmail.com>
2025-05-10 18:30:32 +0530

    resolveing the bugs

--- team-vibe-net/script.js
+++ team-vibe-net/script.js
@@ -2,6 +2,7 @@ const input = document.getElementById("input");
 const count = document.getElementById("count");
 const tweets = document.getElementById("tweets");
 
+
 input.addEventListener("input", () => {
   count.textContent = (140 - input.value.length) + " characters left";
 });
@@ -32,4 +33,5 @@ function postTweet() {
   tweets.insertAdjacentHTML("afterbegin", tweetHTML);
   input.value = "";
   count.textContent = "140 characters left";
+
 }
